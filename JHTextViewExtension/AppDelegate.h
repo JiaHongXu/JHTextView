7cf8238f6c9e8ef7b8763f0137ad199b2a6c907b
Jiahong Xu <xjh_28@163.com>
2016-10-16 13:11:43 +0800

    Initial Commit

---
+++ JHTextViewExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHTextViewExtension
+//
+//  Created by 307A on 2016/10/16.
+//  Copyright © 2016年 徐嘉宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHTextViewExtension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHTextViewExtension
+//
+//  Created by 307A on 2016/10/16.
+//  Copyright © 2016年 徐嘉宏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
